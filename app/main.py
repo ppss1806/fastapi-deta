78b27db18052c7e6556baa7cb94401ff11b05597
ppss1806 <ppss1806@gmail.com>
2022-05-06 03:38:52 +0000

    use poetry and add router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from deta import Deta
+import time
+import os
+
+if os.getenv('DETA_RUNTIME', False):
+    deta = Deta()
+    async_db = deta.AsyncBase("test")
+
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return "Hello World!"
+
+
+@app.get("/myip")
+async def get_myip(request: Request):
+    item = {"ip": request.client.host, "time": time.time()}
+
+    if os.getenv('DETA_RUNTIME', False):
+        await async_db.insert(item)
+
+    return item
